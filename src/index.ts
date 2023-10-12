5e4b27039445cf02a0bead2a5966ad827cd44011
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-10-12 14:49:30 +0800

    remove const uri

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 import router from "./router";
 
@@ -26,11 +27,8 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
 
-const MONGO_URL =
-  "mongodb+srv://raffyamoguis:OsJVh84VagfUkrBZ@cluster0.dozqwsh.mongodb.net/?retryWrites=true&w=majority";
-
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
