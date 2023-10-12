da4c0fa50a5ce003360ee4ec705d6ec12788031b
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-10-12 12:53:14 +0800

    first commit

---
+++ src/middlewares/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import { get, merge } from "lodash";
+
+import { getUserBySessionToken } from "../db/users";
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUser = get(req, "identity._id") as string;
+
+    if (!currentUser) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUser.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    // Check previous cookie
+    const sessionToken = req.cookies["ANTONIO-AUTH"];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
